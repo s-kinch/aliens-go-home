944f3b94f2d04be8cdc51568b89287c5f2e18cc5
sinclaaair <sinclairkinch@gmail.com>
2019-11-04 22:59:41 -0500

    add heart and start button components

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -6,6 +6,8 @@ import CannonPipe from './CannonPipe'
 import CannonBall from './CannonBall'
 import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject'
+import Heart from './Heart'
+import StartGame from './StartGame'
 
 const Canvas = ({ trackMouse, angle }) => {
     const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
@@ -28,6 +30,8 @@ const Canvas = ({ trackMouse, angle }) => {
             <CannonBall position={{ x: 0, y: -100 }} />
             <CurrentScore score={13} />
             <FlyingObject position={{x: -150, y: -300}} />
+            <Heart position={{x: -300, y: 35}} />
+            <StartGame onClick={() => console.log('Aliens')}/>
         </svg>
     )
 }
