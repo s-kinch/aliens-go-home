17e4eb6a9755c607482c3207e4a3eb5e3fe4b5c9
sinclaaair <sinclairkinch@gmail.com>
2019-11-04 21:39:04 -0500

    add svg 'canvas'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Canvas from './components/Canvas'
 
-function App({message}) {
+function App() {
     return (
         <div className="App">
-            {message}
+            <Canvas/>
         </div>
     )
 }
 
 const mapStateToProps = state => ({
-    message: state.message,
+    // message: state.message,
 })
 
 const mapDispatchToProps = {
